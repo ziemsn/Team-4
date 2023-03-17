2f1462e66356a1bbce5381e153e74d1b62d688ad
ziemsn <98898273+ziemsn@users.noreply.github.com>
2023-03-17 14:56:45 -0400

    Update Blade_Saw.h
    Update to read blade state switch, and return blade state when getBladeState() is called

--- Servo_HMI_control/Blade_Saw.h
+++ Servo_HMI_control/Blade_Saw.h
@@ -1,9 +1,10 @@
-
+// Assumes normally-closed blade state switch
 
 #include "ClearCore.h"
 
 #define BLADE_UP 7
 #define BLADE_DOWN 8
+#define Blade_State_Pin D16
 
 int BladeState;
 
@@ -19,7 +20,15 @@ int getBladeState() {
 //Put the bladesaw's state based on switch on and off on ClearCore
 //SET BLADE_UP or BLADE_DOWN
 void detectBladeState() {
+  // Read the state of the limit switch connected to digital I/O 16
+  int switchState = digitalRead(Blade_State_Pin);
 
-
-  
+  // If the switch is not triggered, set BladeState to BLADE_UP
+  if (switchState == LOW) {
+    setBladeState(BLADE_UP);
+  }
+  // If the switch is triggered, set BladeState to BLADE_DOWN
+  else {
+    setBladeState(BLADE_DOWN);
+  }
 }
